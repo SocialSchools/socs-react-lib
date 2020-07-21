4c7022d3a51d2f329efce780340ace017f371dc9
Harry Oosterveen <mail@harryonline.net>
2020-07-21 15:02:35 +0200

    New build

--- src/components/UserText/Emojify/index.js
+++ src/components/UserText/Emojify/index.js
@@ -7,7 +7,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import detectMobile from '../../../utils/detectMobile.lib';
-import Emoji from './Emoji';
+import Emoji from './Loadable';
 
 function Emojify(props) {
   const { children } = props;
