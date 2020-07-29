4c8e5d1af863abeaf2ae884f7f238df6e35e9703
Harry Oosterveen <mail@harryonline.net>
2020-07-29 15:46:25 +0200

    mergeTheme, mergeConfig

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,5 +5,5 @@ export { default as detectMobile } from './detectMobile.lib';
 export * from './strings';
 export { default as useCookie } from './useCookie';
 export * from './useUrl';
-export { mergeTheme, mergeConfig } from './makeTheme';
+export * from './makeTheme';
 export { default as usePathParams } from './usePathParams';
