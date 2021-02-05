1e36166eddef1d657de312568a0325be9e1ebc81
Harry Oosterveen <mail@harryonline.net>
2021-02-05 18:00:15 +0100

    _Blank target for download links in webview

--- src/components/Preview/ListDocuments.js
+++ src/components/Preview/ListDocuments.js
@@ -37,10 +37,11 @@ function ListDocuments(props) {
   if (!files || files.length === 0) {
     return null;
   }
+  const target = window.socsIsInAppWebView ? '_blank' : undefined;
   return (
     <ListGroup>
       {files.map((file) => (
-        <ListGroup.Item action={!onRemove} key={file.id} href={onRemove ? '' : file.fullPath} className="px-1">
+        <ListGroup.Item action={!onRemove} key={file.id} href={onRemove ? '' : file.fullPath} className="px-1" target={target}>
           <LinkWrapper>
             <Icon className={`text-primary ${getTypeIcon(file)}`} />
             <span>{file.fileName}</span>
