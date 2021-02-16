3b95435064ad7aed7843853db467b9ed101eb8f0
Harry Oosterveen <mail@harryonline.net>
2021-02-16 20:56:14 +0100

    test fileType/typeTest

--- src/components/Preview/index.js
+++ src/components/Preview/index.js
@@ -149,7 +149,7 @@ class Preview extends React.PureComponent {
     }
     const parsedFiles = parseFiles(files);
     parsedFiles.media = [...parsedFiles.videos, ...parsedFiles.images];
-    console.log('parsedFiled', parseFiles);
+    console.log('parsedFiled', parsedFiles);
     const MediaWrapper = inline ? Inline : GridView;
     const count = parsedFiles.media.length;
     const forceSquare = inline || small || (count > 1);
